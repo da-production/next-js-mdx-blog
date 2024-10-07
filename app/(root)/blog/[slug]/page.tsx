580a0f4b07d033e7a1e6806f12eab37cb1810e16
MEBROUKI Amine <ami.mebrouki@cnac.local>
2024-10-07 13:57:54 +0100

    updating blog slug page

--- app/(root)/blog/[slug]/page.tsx
+++ app/(root)/blog/[slug]/page.tsx
@@ -1,15 +1,8 @@
-import { unified } from "unified";
-import remarkParse from "remark-parse";
-import remarkRehype from "remark-rehype";
-import rehypeStringify from "rehype-stringify";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { getPost, getTOC } from "@/lib/posts";
 
 
 
 async function page(props:any) {
-    console.log(props);
-    const [post, content] = await getPost(props.params.slug)
     return (
         <MaxWidthWrapper>
             {props.params.slug}
