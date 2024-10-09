546e6e01609cbf518c66bcb445ceb7620637518e
Mebrouki Amine <enimablack@gmail.com>
2024-10-09 16:55:25 +0200

    updating navbar bg color

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -8,8 +8,9 @@ import Logo from './Logo'
 
 function NavBar() {
     return (
-        <nav className='h-16  z-50 sticky bg-background/60 backdrop-blur top-0 border-b px-8  '>
-            <div className='max-w-screen-lg mx-auto h-full gap-5  flex items-center justify-between'>
+        <nav className='h-16  z-50 sticky bg-gradient-to-b overflow-hidden from-green-300 to-background/60 backdrop-blur top-0 border-b px-8 dark:border-slate-800 '>
+            <div className='max-w-screen-lg mx-auto h-full gap-5  flex items-center justify-between relative'>
+                <div className='absolute top-0 left-0 h-full w-full rounded-full bg-white blur-xl dark:bg-slate-900 -z-10'></div>
                 <div className='font-bold text-xl'>
                     <Logo />
                 </div>
